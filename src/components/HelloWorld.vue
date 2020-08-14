acba583361168aaf55613e50d089a82ea03a7fab
Q1sj <q1sj@outlook.com>
2020-08-14 15:28:05 +0800

    vue midea player

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,102 +1,60 @@
 <template>
-  <el-table
-          :data="tableData"
-          style="width: 100%">
-    <el-table-column type="expand">
-      <template slot-scope="props">
-        <el-form label-position="left" inline class="demo-table-expand">
-          <el-form-item label="商品名称">
-            <span>{{ props.row.name }}</span>
-          </el-form-item>
-          <el-form-item label="所属店铺">
-            <span>{{ props.row.shop }}</span>
-          </el-form-item>
-          <el-form-item label="商品 ID">
-            <span>{{ props.row.id }}</span>
-          </el-form-item>
-          <el-form-item label="店铺 ID">
-            <span>{{ props.row.shopId }}</span>
-          </el-form-item>
-          <el-form-item label="商品分类">
-            <span>{{ props.row.category }}</span>
-          </el-form-item>
-          <el-form-item label="店铺地址">
-            <span>{{ props.row.address }}</span>
-          </el-form-item>
-          <el-form-item label="商品描述">
-            <span>{{ props.row.desc }}</span>
-          </el-form-item>
-        </el-form>
-      </template>
-    </el-table-column>
-    <el-table-column
-            label="商品 ID"
-            prop="id">
-    </el-table-column>
-    <el-table-column
-            label="商品名称"
-            prop="name">
-    </el-table-column>
-    <el-table-column
-            label="描述"
-            prop="desc">
-    </el-table-column>
-  </el-table>
+    <video-player  class="video-player vjs-custom-skin"
+                   ref="videoPlayer"
+                   :playsinline="true"
+                   :options="playerOptions"
+                   :headers="headers"
+                   @ready="playerReadied"
+    ></video-player>
 </template>
 
-<style>
-  .demo-table-expand {
-    font-size: 0;
-  }
-  .demo-table-expand label {
-    width: 90px;
-    color: #99a9bf;
-  }
-  .demo-table-expand .el-form-item {
-    margin-right: 0;
-    margin-bottom: 0;
-    width: 50%;
-  }
-</style>
-
 <script>
-  export default {
-    data() {
-      return {
-        tableData: [{
-          id: '12987122',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }, {
-          id: '12987123',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }, {
-          id: '12987125',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }, {
-          id: '12987126',
-          name: '好滋好味鸡蛋仔',
-          category: '江浙小吃、小吃零食',
-          desc: '荷兰优质淡奶，奶香浓而不腻',
-          address: '上海市普陀区真北路',
-          shop: '王小虎夫妻店',
-          shopId: '10333'
-        }]
-      }
+    import 'video.js/dist/video-js.css'
+    import 'vue-video-player/src/custom-theme.css'
+    export default {
+        data(){
+            return{
+                playerOptions : {
+                    // playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
+                    autoplay: false, //如果true,浏览器准备好时开始回放。
+                    muted: false, // 默认情况下将会消除任何音频。
+                    loop: false, // 导致视频一结束就重新开始。
+                    preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+                    language: 'zh-CN',
+                    aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+                    fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+                    sources: [{
+                        type: "application/x-mpegURL",//这里的种类支持很多种：基本视频格式、直播、流媒体等，具体可以参看git网址项目
+                        src: "http://192.168.1.7:18080/v1/hls/test.m3u8" //url地址
+                    }],
+                    // poster: "../../static/images/test.jpg", //你的封面地址
+                    // width: document.documentElement.clientWidth, //播放器宽度
+                    notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+                    controlBar: {
+                        timeDivider: true,
+                        durationDisplay: true,
+                        remainingTimeDisplay: false,
+                        fullscreenToggle: true  //全屏按钮
+                    }
+                }
+            }
+        },
+        methods:{
+            playerReadied (player) {
+                let _this = this
+                var hls = player.tech({IWillNotUseThisInPlugins: true}).hls
+                player.tech_.hls.xhr.beforeRequest = function (options) {
+                    // console.log(options)
+                    const token = localStorage.getItem('Authorization');
+                    options.headers = {
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json;charset=UTF-8',
+                        'Accept-Language': 'zh-CN,zh;q=0.9,hy;q=0.8,mn;q=0.7',
+                        'Authorization': 'Ticket '+token
+                    }
+                    return options
+                }
+            }
+        }
     }
-  }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import VideoPlayer from 'vue-video-player'
+import hls from 'videojs-contrib-hls'
+import "video.js/dist/video-js.css"
+import "vue-video-player/src/custom-theme.css"
 
+Vue.use(VideoPlayer)
+Vue.use(hls)
 Vue.config.productionTip = false
 
 new Vue({
